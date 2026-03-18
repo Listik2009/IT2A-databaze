f0d53090626e8e06edbd6e01983d3ed197b161eb
Listik2009 <61786bui@student.sstebrno.cz>
2026-03-18 12:39:15 +0100

    Add files via upload

---
+++ evidence_hrace/WpfApp2/WpfApp2/Hrac.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    namespace EvidenceHracu
+    {
+        public class Hrac
+        {
+            public int ID { get; set; }
+            public string Jmeno { get; set; }
+            public string Tym { get; set; }
+            public int Vek { get; set; }
+            public int PocetGolu { get; set; }
+            public bool JeAktivni { get; set; }
+        }
+    }
+}
